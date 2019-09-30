56ed4747ff7ccd55a85273df528c95981c16a893
ManuGNR <manurivasgnr@gmail.com>
2019-09-30 16:46:17 -0300

    se agrego el nombre del usuario logeado para que lo muestre el header.tpl en la navbar

--- view/View.php
+++ view/View.php
@@ -4,12 +4,18 @@
   class View {
 
     protected $basehref;
-    protected $smarty;    
+    protected $smarty;
+    protected $nombreUsuario; 
     
     public function __construct() {
-        $this->basehref = '//'.$_SERVER['SERVER_NAME'].dirname($_SERVER['PHP_SELF']).'/'; 
+        session_start();
+        if(isset($_SESSION['email'])) {
+          $this->nombreUsuario = $_SESSION['email'];
+        }
+        $this->basehref = '//'.$_SERVER['SERVER_NAME'].dirname($_SERVER['PHP_SELF']).'/';
         $this->smarty = new Smarty();
-        $this->smarty->assign('basehref',$this->basehref);        
+        $this->smarty->assign('basehref',$this->basehref);
+        $this->smarty->assign('nombreUsuario',$this->nombreUsuario);        
     }
   }
  ?>
